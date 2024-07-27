2835459fc7893d455b12c70b70ae23ca69efe696
Satyam Kumar Jha <satyamjha23112001@gmail.com>
2024-07-28 01:22:34 +0530

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Quote from 'inspirational-quotes';
 
 // fetch("https://jsonplaceholder.typicode.com/todos/1")
 //   .then((response) => response.json())
@@ -8,3 +9,6 @@ axios.get("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => {
     console.log(response.data);
   });
+
+
+  console.log(Quote.getQuote());
